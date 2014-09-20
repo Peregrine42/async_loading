f917bf42f39b11f83fe0e0f34ed0e84352db185b
Duncan Fermor <duncan.fermor@gmail.com>
2014-09-20 22:36:46 +0100

    version 2 - more informative

--- spec/controllers/results_controller_spec.rb
+++ spec/controllers/results_controller_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe 'GET /results' do
   it 'shows the results' do
+    a_referendum = double(:ref, result: false, for: 4, against: 5)
+    allow(Referendum).to receive(:find).and_return(a_referendum)
     get '/results'
     expect(last_response.body).to match 'No'
   end
