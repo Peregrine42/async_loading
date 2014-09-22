fdd81c70f0e54afc12d019c636abf783369d85d3
Duncan Fermor <duncan.fermor@gmail.com>
2014-09-22 21:43:59 +0100

    404 page and redirects complete

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -9,4 +9,14 @@ class App
     erb :loading
   end
 
+  get '/referendum_results/:id' do
+    @ref = ReferendumResult.where(referendum_id: params[:id]).first
+    if @ref
+      erb :results
+    else
+      status 404
+      '404 - result not found'
+    end
+  end
+
 end
